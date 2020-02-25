a3e57175e11d08b5650690edf0c6cb783e3bd1fb
qdeca <qdecayeux@natsystem.fr>
2020-02-25 11:30:39 +0100

    méthode de login

--- firstProject/src/fr/abm/firstproject/LoginMain.java
+++ firstProject/src/fr/abm/firstproject/LoginMain.java
@@ -5,26 +5,39 @@ import java.util.Scanner;
 
 public class LoginMain {
 	
+	
+	private static final String CORRECT_USERNAME = "admin";
+	private static final String CORRECT_PASSWORD = "admin";
+
+
+
+
 	public static void main(String[] args) {
+		try {
+			login();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+
+
+
+	private static void login() throws Exception {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Veuillez entrer votre username :");
 		String username = scan.nextLine();
-		System.out.println("Votre username est " + username);
-
-		try {
-			saisieAge();
-		} catch (InputMismatchException e) {
-			System.out.println("Mauvaise saisie");
-			throw new InputMismatchException();
-		} catch (NullPointerException e) {
-			System.out.println("null");
-		} catch (Exception e) {
-			System.out.println("Une exception est survenue");
-		} finally {
-			System.out.println("Fin du questionnaire");
-		}
+		System.out.println("Veuillez entrer votre mot de passe :");
+		String password = scan.nextLine();
 		scan.close();
+		if (CORRECT_USERNAME.equals(username) && CORRECT_PASSWORD.equals(password)) {
+			System.out.println("Bienvenue !");
+		} else {
+			throw new Exception("Problème dans le login");
+		}
 	}
+	
+	
 	
 	
 	private static void saisieAge() throws InputMismatchException {
